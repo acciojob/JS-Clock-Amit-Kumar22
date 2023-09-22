528f2a2ca03efd8cf404098a3b45201fa510590c
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-22 10:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function updateClock() {
+    const hourHand = document.getElementById("hour-hand");
+    const minuteHand = document.getElementById("minute-hand");
+    const secondHand = document.getElementById("second-hand");
+
+    const now = new Date();
+    const hours = now.getHours() % 12;
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourDeg = (360 / 12) * hours + (360 / 12) * (minutes / 60);
+    const minuteDeg = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
+    const secondDeg = (360 / 60) * seconds;
+
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+}
+
+setInterval(updateClock, 1000); // Update the clock every second
